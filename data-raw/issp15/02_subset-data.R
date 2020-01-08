0168f3928e6f79a30cde12673a3f6caf69fb876a
Hansjoerg Plieninger <plieninger@uni-mannheim.de>
2020-01-08 11:29:56 +0100

    remove 2 duplicate cases
    * ISSP announced in the document 'ZA1490_All_Overview_Duplicated Records.xlsx' on https://doi.org/10.4232/1.12848 that 6 duplicate cases where identified
    * 2 out of 6 were included in my previous analyses
    * The exclusion does not change the results substantially and the conclusions remain the same

--- data-raw/issp15/02_subset-data.R
+++ data-raw/issp15/02_subset-data.R
@@ -4,6 +4,20 @@ conflict_prefer("filter", "dplyr")
 
 data("issp15")
 
+# The method for sampling random numbers in sample() was changed in R version 3.6.0.
+# The subsamples drawn here were initially drawn using an earlier R version.
+# Thus, to reproduce the old behavior, a different sampling method has to be
+# used via RNGversion. See ?RNGkind for further details.
+RNGversion("3.5.2")
+
+# Duplicated cases --------------------------------------------------------
+
+# The document "ZA1490_All_Overview_Duplicated Records.xlsx" marks the following
+# six cases from Venezuela (two of which were included in my previous analyses)
+# as duplicates:
+# "The archive recommends deleting all the six affected cases."
+
+dplcts <- "2015008620000053|2015008620000054|2015008620000123|2015008620000127|2015008620000148|2015008620000149"
 
 # Random subsample of countries -------------------------------------------
 
@@ -24,15 +38,18 @@ ctrs <- sample(levels(issp15$country), 10) %>%
 
 set.seed(1234)
 id_subset <- issp15 %>%
-    select(v45:v47, country, rowid) %>%
+    select(v45:v47, country, rowid, CASEID) %>%
     filter(., complete.cases(.)) %>%
     split(., .$country) %>%
     purrr::map_if(., ~ nrow(.x) >= 500, ~ sample_n(.x, 500)) %>%
     bind_rows() %>%
+    filter(!grepl(dplcts, CASEID)) %>%
     pull(rowid)
 
 # issp15 %>%
 #     filter(rowid %in% id_subset) %>%
 #     filter(country %in% ctrs)
 
-usethis::use_data(ctrs, id_subset)
+usethis::use_data(ctrs, id_subset, overwrite = TRUE)
+
+RNGversion(paste(R.Version()$major, R.Version()$minor, sep = "."))
